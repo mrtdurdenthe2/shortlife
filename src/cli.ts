75af2074db3e5953bb2e8f7c2035abf551eb4c36
mrtdurdenthe2 <kai.wil.business@gmail.com>
2025-11-27 22:12:12 +0000

    made cli file

---
+++ src/cli.ts
@@ -0,0 +1,11 @@
+import { Effect } from "effect";
+import { Args, Command, Options } from "@effect/cli";
+import { BunContext, BunRuntime } from "@effect/platform-bun";
+// no args = view age
+// --set == set age
+// --bd == set birthday
+//
+// > shortlife
+// Your current set birthday is {}
+
+
