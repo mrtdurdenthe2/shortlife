7ed8e6bc36b9da6cbc5aad111978686c465d3f83
mrtdurdenthe2 <kai.wil.business@gmail.com>
2025-11-28 00:29:30 +0000

    setup the setup command

--- src/cli.ts
+++ src/cli.ts
@@ -1,13 +1,19 @@
-import { Effect } from "effect";
+import { Effect, Console} from "effect";
 import { Args, Command, Options } from "@effect/cli";
 import { BunContext, BunRuntime } from "@effect/platform-bun";
+import { atLeast } from "@effect/cli/Options";
 // no args = view age
 // --set == set age
 // --bd == set birthday
 //
 // > shortlife
 // Your current set birthday is {}
-//
 
 
+const Idate = Options.text("date").pipe(Options.withAlias("d"))
 
+const Iage = Options.text("age").pipe(Options.withAlias("a"))
+
+const setup = Command.make("date", { Idate, Iage}, ({ Idate, Iage }) =>
+ Console.log("TEST") 
+);
